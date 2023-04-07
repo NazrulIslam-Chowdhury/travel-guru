6f3181c80a16d6e03065474c6c0e4fd4646a8b96
rahiislam96 <nazrulislam66188@gmail.com>
2023-04-07 11:21:02 +0600

    booking form

--- src/components/booking/Booking.js
+++ src/components/booking/Booking.js
@@ -5,32 +5,58 @@ const Booking = () => {
     return (
         <div className='bg2'>
             <Header />
-            <div className="relative flex justify-center p-[10%]">
-                <div className="relative flex justify-center rounded-md w-[400px] min-h-[400px] bg-[rgba(255, 255, 255, 0.1)] backdrop-blur-md shadow-2xl border-solid border-r-2 border-b-2 border-gray-400">
-                    <div className="form">
-                        <h2>Booking</h2>
-                        <form action="">
-                            <div className="inputBox">
+            <div className="relative flex justify-center p-[2%]">
+                <div className="relative flex justify-center rounded-md w-[400px] min-h-[400px] bg-[rgba(255, 255, 255, 0.1)] backdrop-blur-md shadow-2xl shadow-black border-solid border-r-2 border-b-2 border-gray-400">
+                    <div className="relative w-full h-full pt-5 pr-20 pb-5 pl-12">
+                        <h2 className='font1 text-white text-2xl font-bold mb-10 tracking-wide before:absolute before:left-12 before:top-14 before:w-28 before:h-1 before:bg-white'>Booking</h2>
+                        <form action="" className='font1'>
+                            <div className="w-full mt-3">
                                 <input
                                     type="text"
-                                    placeholder='Name' />
+                                    placeholder='Name'
+                                    className='w-full px-5 py-2 rounded border-solid border-r-2 border-[rgba(255, 255, 255, 0.2)] border-b-2 outline-none bg-gray-500 text-lg tracking-wide text-white shadow-sm shadow-black placeholder:text-white' />
                             </div>
-                            <div className="inputBox">
+                            <div className="mt-3">
                                 <input
                                     type="email"
-                                    placeholder='Email' />
+                                    placeholder='Email'
+                                    className='w-full px-5 py-2 rounded border-solid border-r-2 border-[rgba(255, 255, 255, 0.2)] border-b-2 outline-none bg-[rgba(255, 255, 255, 0.2)] text-lg tracking-wide text-white bg-gray-500 shadow-md shadow-black placeholder:text-white'
+                                />
                             </div>
-                            <div className="inputBox">
+                            <div className="mt-3">
                                 <input
                                     type="tel"
-                                    placeholder='Mobile' />
+                                    placeholder='Mobile'
+                                    className='w-full px-5 py-2 rounded border-solid border-r-2 border-[rgba(255, 255, 255, 0.2)] border-b-2 outline-none bg-[rgba(255, 255, 255, 0.2)] text-lg tracking-wide text-white bg-gray-500 shadow-md shadow-black placeholder:text-white'
+                                />
                             </div>
-                            <div className="inputBox">
+                            <div className="mt-3">
                                 <input
-                                    type="date" />
+                                    type="tel"
+                                    placeholder='Destination'
+                                    className='w-full px-5 py-2 rounded border-solid border-r-2 border-[rgba(255, 255, 255, 0.2)] border-b-2 outline-none bg-[rgba(255, 255, 255, 0.2)] text-lg tracking-wide text-white bg-gray-500 shadow-md shadow-black placeholder:text-white'
+                                />
                             </div>
-                            <div className="inputBox">
-                                <input type="submit" value='Submit' className='bg-sky-400 text-white w-full' />
+                            <div className='mt-3'>
+                                <label htmlFor=""
+                                    className='font1 text-white mt-4'
+                                >From</label>
+                                <input
+                                    type="date"
+                                    className='w-full px-5 py-2 rounded border-solid border-r-2 border-[rgba(255, 255, 255, 0.2)] border-b-2 outline-none bg-[rgba(255, 255, 255, 0.2)] text-lg tracking-wide text-white bg-gray-500 shadow-md shadow-black mt-3 placeholder:text-white'
+                                />
+                            </div>
+                            <div className="mt-3">
+                                <label htmlFor=""
+                                    className='font1 text-white mt-4'
+                                >To</label>
+                                <input
+                                    type="date"
+                                    className='w-full px-5 py-2 rounded border-solid border-r-2 border-[rgba(255, 255, 255, 0.2)] border-b-2 outline-none bg-[rgba(255, 255, 255, 0.2)] text-lg tracking-wide text-white bg-gray-500 shadow-md shadow-black placeholder:text-white'
+                                />
+                            </div>
+                            <div className="mt-3">
+                                <input type="submit" value='Submit' className='bg-sky-400 text-white w-full cursor-pointer p-3 mt-3 mb-5 font-bold rounded' />
                             </div>
                         </form>
                     </div>
